97fe79e4d423d4da5b1e4076107c0f7bc7447a96
Alix Crusoe <acrusoe@f2r2s8.clusters.42paris.fr>
2025-11-17 11:08:39 +0100

    first

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<string, double> poids; //Une table qui associe le nom d'un animal à son poids
+    
+    //On ajoute les poids de quelques animaux
+    poids["souris"] = 0.05;
+    poids["tigre"] = 200;
+    poids["chat"] = 3;
+    poids["elephant"] = 10000;
+
+    //Et on parcourt la table en affichant le nom et le poids
+    for(map<string, double>::iterator it=poids.begin(); it!=poids.end(); ++it)
+    {
+        cout << it->first << " pese " << it->second << " kg." << endl;
+    }
+    return 0;
+}
