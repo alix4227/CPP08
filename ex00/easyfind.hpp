904e50e25b6a1e42bfa8544d16adb6774a4fa5a4
Alix Crusoe <acrusoe@f4r13s12.clusters.42paris.fr>
2026-01-12 16:14:17 +0100

    fix ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -8,19 +8,21 @@
 #include <iostream>
 
 template <typename T>
+typename T::iterator	easyfind(T& tab, int nbr)
+{
+	for(typename T::iterator it=tab.begin(); it!=tab.end(); ++it)
+	{
+		if (*it == nbr)
+			return (it);
+	}
+	return (tab.end());
+}
+
 // bool	easyfind(const T& tab, int nbr)
 // {
-// 	vector<int>nb(nbr);
-// 	if (find(tab.begin(), tab.end(), nbr) != tab.end())
+// 	std::vector<int>nb(1,nbr);
+// 	if (find_first_of(tab.begin(), tab.end(), nb.begin(), nb.end()) != tab.end())
 // 		return (true);
 // 	return (false);
 // }
-
-bool	easyfind(const T& tab, int nbr)
-{
-	std::vector<int>nb(1,nbr);
-	if (find_first_of(tab.begin(), tab.end(), nb.begin(), nb.end()) != tab.end())
-		return (true);
-	return (false);
-}
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,9 +3,13 @@
 int	main(void)
 {
 	int nbr = 2;
-	std::vector<int>tab = {-111111, 2, 3, 4};
-	if (easyfind(tab, nbr))
-		std::cout << "first occurence of integer " << nbr << std::endl;
+	std::vector<int>tab = {-1, 3, 0, 5, 4};
+	std::vector<int>::iterator it = easyfind(tab, nbr);
+	if (it != tab.end())
+	{
+		int position = std::distance(tab.begin(), it);
+		std::cout << "first occurence of integer " << nbr  << " found at position " << position << std::endl;
+	}
 	else
 		std::cout << "no occurence of nbr found" << std::endl;
 	return (0);
